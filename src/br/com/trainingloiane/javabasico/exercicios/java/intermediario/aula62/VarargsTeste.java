7720fb0ee031095a85eb8599ef48616a1cd735f5
dsb88 <diego.brandao@outlook.com>
2021-08-03 00:54:36 -0300

    first commit

--- src/br/com/trainingloiane/javabasico/exercicios/java/intermediario/aula62/VarargsTeste.java
+++ src/br/com/trainingloiane/javabasico/exercicios/java/intermediario/aula62/VarargsTeste.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 
 public class VarargsTeste {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-        int[] vet = new int[3];
+        Scanner sc = new Scanner(System.in);
 
-        for(int i=0; i< vet.length; i++){
-            vet[i]=sc.nextInt();
-        }
-
-        System.out.println(soma(vet));
+        System.out.println(soma(4,58,9,3,4,6,159,258,3));//Método Varargs
 
     }
 
+//    static int soma(int a, int b) {
+//        return a + b;
+//    }
+//
+//    static int soma(int[] vetor) {
+//        int total = 0;
+//
+//        for (int i : vetor) {
+//            total += i;
+//        }
+//        return total;
+//    }
 
-    static int soma(int a, int b) {
-        return a + b;
-    }
-
-    static int soma(int[] vetor) {
+    static int soma(Integer... vet) {
         int total = 0;
 
-        for (int i : vetor) {
+        for (int i : vet) {
             total += i;
         }
         return total;
